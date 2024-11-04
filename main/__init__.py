ef984a011cf200054a110b29213bbc4d636b4f16
jperc18 <154987530+JP3-Code@users.noreply.github.com>
2024-11-03 19:32:57 -0500

    Fix error when reading CSV file.

--- main/__init__.py
+++ main/__init__.py
@@ -3,13 +3,9 @@ import csv
 # Creates hash table
 class HashTable:
     def __init__(self, initial_capacity=10):
-        self.table = [None] * initial_capacity
-        #for i in range(initial_capacity):
-            #self.table.append([])
-
-    # Maps values to key
-    def hash_function(self, key):
-        return hash(key) % self.size
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
 
     # Inserts data into the hash table
     def insert(self, key, value):
@@ -17,13 +13,12 @@ class HashTable:
         bucket_list = self.table[bucket]
 
         # Updates key if already in the bucket
-        for kv in bucket_list:
-            if kv[0] == key:
-                kv[1] = value
+        for item in bucket_list:
+            if item[0] == key:
+                item[1] = value
                 return True
 
-        key_value = [key, value]
-        bucket_list.append(key_value)
+        bucket_list.append([key,value])
         return True
 
     # Looks for item with matching key
@@ -31,35 +26,10 @@ class HashTable:
         bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
 
-        for kv in bucket_list:
-            if kv[0] == key:
-                return kv[1]
+        for item in bucket_list:
+            if item[0] == key:
+                return item[1]
             return None
-
-
-def load_package_data(filename):
-    with open(filename) as wgupsPackageFile:
-        packageData = csv.reader(wgupsPackageFile, delimiter=',')
-        for package in packageData:
-            pID = int(package[0])
-            pAddress = package[1]
-            pCity = package[2]
-            pState = package[3]
-            pZipcode = int(package[4])
-            pDeadline = package[5]
-            pWeight = int(package[6])
-            pStatus = package[7]
-
-            # Package object
-            p = Package(pID, pAddress, pCity, pState, pZipcode, pDeadline, pWeight, pStatus)
-
-            # Insert into the hash table
-            myHashTable.insert(pID, p)
-
-
-# Hash Table instance
-myHashTable = HashTable()
-
 
 class Package:
     def __init__(self, package_id, address, city, state, zip_code, deadline, pk_weight, status):
@@ -72,24 +42,54 @@ class Package:
         self.pk_weight = pk_weight
         self.status = status
 
-        def __str__(self):
-            return "%s, %s, %s, %s, %s, %s, %s, %s" % (
-            self.package_id, self.address, self.city, self.state, self.zip_code, self.deadline, self.pk_weight,
-            self.status)
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s" % (
+        self.package_id, self.address, self.city, self.state, self.zip_code, self.deadline, self.pk_weight,
+        self.status)
 
+        #def __str__(self):
+            #return f"ID: {self.package_id}, Address: {self.address}, {self.city}, {self.state}, {self.zip_code}, Deadline: {self.deadline}, Weight: {self.pk_weight}, Status: {self.status}"
 
 class Truck:
     def __init__(self, truck):
         self.truck = truck
 
 
+def loadPackageData(filename):
+    with open(filename) as wgupsPackageFile:
+        packageData = csv.reader(wgupsPackageFile, delimiter=',')
+        next(packageData)
+        for packages in packageData:
+            pID = int(packages[0])
+            pAddress = packages[1]
+            pCity = packages[2]
+            pState = packages[3]
+            pZipcode = int(packages[4])
+            pDeadline = packages[5]
+            pWeight = int(packages[6])
+            pStatus = packages[7]
+
+            # Package object
+            packages = Package(pID, pAddress, pCity, pState, pZipcode, pDeadline, pWeight, pStatus)
+
+            # Insert into the hash table
+            myHashTable.insert(pID, packages)
+
+
+# Hash Table instance
+myHashTable = HashTable(40)
+
 # Load packages into Hash Table
-load_package_data('WGUPSPackageFile.csv')
+loadPackageData('WGUPSPackageFile.csv')
 
-print('Packages:')
+print('Delivery Information:')
 
-for i in range(len(myHashTable.table)):
-    package = myHashTable.lookup(myHashTable.table[i])
+# Remove after debugging
+total = 0
+
+for i in range (1, len(myHashTable.table)+1):
+    package = myHashTable.lookup(i)
     if package is not None:
-        print('Package ID: {}'.format(package))
+
+        print("Package Details: {}".format(package))
 
